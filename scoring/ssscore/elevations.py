2e0d5e9041c0134c34958650487ba28583daa462
Eugene Ciurana <eugenex@cime.net>
2019-04-18 23:41:21 -0700

    Tactical commit.

--- scoring/ssscore/elevations.py
+++ scoring/ssscore/elevations.py
@@ -37,13 +37,13 @@
 
 # Drop zone name, altitude in meters
 
-BAY_AREA_SKYDIVING           =  23.90
-CHICAGOLAND_SKYDIVING_CENTER = 238.00
-SKYDANCE_SKYDIVING           =  30.48
-SKYDIVE_ALGARVE              =   2.00
-SKYDIVE_ARIZONA              = 460.60
-SKYDIVE_BUZZ                 = 256.00
-SKYDIVE_FANO                 =  54.00
-SKYDIVE_FLANDERS_ZWARTBERG   =  85.00
-SKYDIVE_SAULGAU              = 580.00
+BAY_AREA_SKYDIVING           =   23.90
+CHICAGOLAND_SKYDIVING_CENTER =  238.00
+SKYDANCE_SKYDIVING           =   30.48
+SKYDIVE_ALGARVE              =    2.00
+SKYDIVE_ARIZONA              =  460.60
+SKYDIVE_BUZZ                 =  256.00
+SKYDIVE_FANO                 =   54.00
+SKYDIVE_FLANDERS_ZWARTBERG   =   85.00
+SKYDIVE_SAULGAU              = 1076.00
 
