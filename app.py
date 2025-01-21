f4baffdb07949780e44f0d25312974b9c1b3185f
Eugene Ciurana <github@cime.net>
2025-01-21 10:06:16 -0800

    Defining explicit Python path.

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 # See: https://github.com/pr3d4t0r/SSScoring/blob/master/LICENSE.txt
 
 
-# from ssscoring.app import main
-
 import os
 import pathlib
 
+dirPath = pathlib.Path('.')
+os.environ['PYTHONPATH'] = dirPath.absolute()
+
 import streamlit as st
+
+from ssscoring.app import main
 
 
 if '__main__' == __name__:
     st.write(os.environ)
-    dirPath = pathlib.Path('.')
     st.write('Path = %s' % dirPath.absolute())
     for entry in dirPath.iterdir():
         st.write(entry)
