36991010c0ce39ee5b4e3aa5ccd6b7f1ac44a8ba
Eugene Ciurana <github@cime.net>
2025-03-03 08:33:58 +0700

    Identified the issue.

--- debugging-harness.py
+++ debugging-harness.py
@@ -10,7 +10,7 @@ from haversine import Unit
 from ssscoring.calc import aggregateResults
 from ssscoring.calc import convertFlySight2SSScoring
 from ssscoring.calc import dropNonSkydiveDataFrom
-from ssscoring.calc import getFlySightDataFromCSV
+from ssscoring.calc import getFlySightDataFromCSVFileName
 from ssscoring.calc import getSpeedSkydiveFrom
 from ssscoring.calc import isValidMinimumAltitude
 from ssscoring.calc import jumpAnalysisTable
@@ -41,12 +41,12 @@ import pandas as pd
 
 DATA_LAKE_ROOT = './data'
 
-dropZoneAltMSL = 1509.0
+dropZoneAltMSL = 10
 # dropZoneAltMSL = 15.0
 dropZoneAltMSLMeters = dropZoneAltMSL/FT_IN_M
 jumpFiles = getAllSpeedJumpFilesFrom(DATA_LAKE_ROOT)
 filePath = list(jumpFiles.keys())[0]
-rawData, tag = getFlySightDataFromCSV(filePath)
+rawData, tag = getFlySightDataFromCSVFileName(filePath)
 data = convertFlySight2SSScoring(rawData, altitudeDZMeters=dropZoneAltMSLMeters)
 jumpResult = processJump(data)
 
